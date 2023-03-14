429382df450e81e0da9de3750c849857ed321adb
GeorgeCheng1023 <ck40292@gmail.com>
2023-03-14 21:54:24 +0800

    feat: basic navLink

--- src/components/Header/Navigation/index.tsx
+++ src/components/Header/Navigation/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 function Navigation() {
   return (
     <nav className="nav">
-      Navigation
+      <NavLink to="/">搜尋活動</NavLink>
+      <NavLink to="/">熱門活動</NavLink>
+      <NavLink to="/">活動分類</NavLink>
     </nav>
   );
 }
