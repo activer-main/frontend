f1465858471ae141bade4abf7bb1a8d06352fbcd
GeorgeCheng1023 <ck40292@gmail.com>
2023-03-14 21:29:12 +0800

    feat: init router

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import routerConfig from 'utils/router';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter(routerConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
---
+++ src/utils/router.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+  RouteObject,
+} from 'react-router-dom';
+
+const routerConfig: RouteObject[] = [
+  {
+    path: '/',
+    element: <div>Hello world!</div>,
+  },
+];
+
+export default routerConfig;
