1d30190f7e969c25dfc7417aa52f9450d3ff3c01
GeorgeCheng1023 <ck40292@gmail.com>
2023-03-17 00:23:05 +0800

    feat: button basic

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,11 @@
+import Button from 'components/Button';
 import React from 'react';
 
 function Home() {
   return (
     <div className="home">
       <h1>Home</h1>
+      <Button text="test" />
     </div>
   );
 }
