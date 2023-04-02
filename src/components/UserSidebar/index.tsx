fc3dc92152c667e01910e9c2e138e0fd65b91827
GeorgeCheng(Laptop) <ck40292@gmail.com>
2023-04-02 21:35:58 +0800

    feat: user side bar

--- src/components/UserSidebar/index.tsx
+++ src/components/UserSidebar/index.tsx
@@ -3,9 +3,10 @@ import { FaUserAlt } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
 import { useAppDispatch } from 'store';
 import { logout } from 'store/auth/authSlice';
-
-import './index.scss';
+import { AiFillHome } from 'react-icons/ai';
+import { RiTaskFill, RiLogoutBoxFill } from 'react-icons/ri';
 import UserSideBarItem from './UserSidebarItem';
+import './index.scss';
 
 function UserSideBar() {
   const dispatch = useAppDispatch();
@@ -20,18 +21,23 @@ function UserSideBar() {
   return (
     <div className="user-sidebar">
       <UserSideBarItem
+        label="首頁"
+        icon={<AiFillHome />}
+        to="/"
+      />
+      <UserSideBarItem
         label="基本資料"
         icon={<FaUserAlt />}
         to="profile"
       />
       <UserSideBarItem
         label="活動管理"
-        icon={<FaUserAlt />}
+        icon={<RiTaskFill />}
         to="manage?"
       />
       <UserSideBarItem
         label="登出"
-        icon={<FaUserAlt />}
+        icon={<RiLogoutBoxFill />}
         to="/"
         onClick={handleLogout}
       />
