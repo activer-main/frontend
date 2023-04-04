a188bd5050a74867efd72de7eb48d768a434567c
onandon <047633597q@gmail.com>
2023-04-04 22:54:48 +0800

    test: login process
    mock redux to test login

--- src/pages/Login/__tests__/Login.test.tsx
+++ src/pages/Login/__tests__/Login.test.tsx
@@ -1,131 +1,128 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
+import configureStore from 'redux-mock-store';
 import mockAxios from 'jest-mock-axios';
 
-import React from 'react';
+import { UserDataType, UserInfoType } from 'types/user';
 import userEvent from '@testing-library/user-event';
-import { render, screen } from '@testing-library/react';
 
-import { MemoryRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import configureMockStore from 'redux-mock-store';
-
-import { LoginRequestType } from 'types/request';
-import { userLogin } from 'store/auth/authAction';
-import { LoginResponseType } from 'types/response';
 import Login from '../index';
 
-// initial state in redux
-const mockStore = configureMockStore();
-const store = mockStore({
-  auth: {
-    loading: false,
-    userInfo: null,
-    userToken: null,
-    error: null,
-    success: false,
-  },
-});
+// server url
+const IP = '220.132.244.41';
+const PORT = '5044';
 
-async function renderComponent() {
+export const URL = `http://${IP}:${PORT}`;
+
+// mock user data
+const mockUserData: UserInfoType = {
+  id: 1,
+  email: '',
+  verify: false,
+  realName: 'onandon',
+  nickName: 'onandon',
+  avatar: 'onandon',
+  gender: 'onandon',
+  birthdat: new Date(2020, 6, 9),
+  profession: 'onandon',
+  phone: 'onandon',
+  county: 'onandon',
+  area: 'onandon',
+  activityHistory: [],
+  tagHistory: [],
+};
+
+// render login component
+function renderComponent() {
+  const initialState: UserDataType = {
+    loading: false,
+    userInfo: null, // for user object
+    userToken: null, // for storing the JWT
+    error: null,
+    success: false, // for monitoring the registration process.
+  };
+
+  const mockStore = configureStore([]);
+  const store: ReturnType<typeof mockStore> = mockStore(initialState);
+
   render(
     <Provider store={store}>
-      <MemoryRouter initialEntries={['/login']}>
+      <MemoryRouter>
         <Login />
       </MemoryRouter>
     </Provider>,
   );
 }
 
-describe('user is not sign in', () => {
-  beforeEach(() => {
-  });
+describe('When user is not login', () => {
   afterEach(() => {
+    // cleaning up the mess left behind the previous test
     mockAxios.reset();
   });
 
-  it('should display sign in button', async () => {
-    // render
-    await renderComponent();
+  it('Login page should renders correctly', () => {
+    renderComponent();
 
-    // find sign in button
-    const signButton = screen.getByRole('button', { name: '登入' });
-
-    // assertion
-    expect(signButton).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: '登入' })).toBeInTheDocument();
+    expect(screen.getByRole('email')).toBeInTheDocument();
+    expect(screen.getByLabelText('密碼')).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: '登入' })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: '註冊' })).toBeInTheDocument();
   });
 
-  it('should register button can go to register page', async () => {
-    // render
-    await renderComponent();
+  it('Submits form data correctly', () => {
+    // setup login body
+    const email = 'test@example.com';
+    const password = 'Test123!';
 
-    // user click register button
-    const signupButton = screen.getByRole('button', { name: '註冊' });
+    // mock catch fn
+    const catchFn = jest.fn();
 
-    // assertion user in register page
-    expect(signupButton).toBeInTheDocument();
+    // render component
+    renderComponent();
+
+    // simulate user login process
+    userEvent.type(screen.getByRole('email'), email);
+    userEvent.type(screen.getByLabelText('密碼'), password);
+    userEvent.click(screen.getByRole('button', { name: '登入' }));
+
+    // assert login api is called
+    expect(mockAxios.post).toHaveBeenCalledWith(
+      `${URL}/auth/signin`,
+      { email, password },
+    );
+
+    // assert api don't have error
+    expect(catchFn).not.toHaveBeenCalled();
   });
 });
 
-describe('when user signs in', () => {
-  beforeEach(() => {
-  });
+// =====================
 
-  it('should call postLogin function on submit button click', async () => {
-    // setup login body
-    const loginBody: LoginRequestType = {
-      email: 'test@example.com',
-      password: 'Password1!',
-    };
-
-    // setup login response
-    const loginResponse: LoginResponseType = {
-      user: {
-        id: 1,
-        email: '',
-        verify: false,
-        realName: '',
-        nickName: '',
-        avatar: '',
-        gender: '',
-        birthdat: new Date(2013, 2, 1, 1, 10),
-        profession: '',
-        phone: '',
-        county: '',
-        area: '',
-        activityHistory: [],
-        tagHistory: [],
+describe('When user login', () => {
+  it('redirects to profile page if user is authenticated', () => {
+    const mockStore = configureStore([]);
+    const store = mockStore({
+      auth: {
+        loading: false,
+        userInfo: mockUserData, // for user object
+        userToken: 'testToken', // for storing the JWT
+        error: null,
+        success: false, // for monitoring the registration process.
       },
-      token: {
-        accessToken: 'token',
-        expireIn: 123,
-      },
-    };
+    });
 
-    // Render Login component
-    renderComponent();
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <Login />
+        </MemoryRouter>
+      </Provider>,
+    );
 
-    // Fill in form fields
-    const emailInput = screen.getByRole('textbox', { name: '帳號' });
-    const passwordInput = screen.getByLabelText('密碼');
-    userEvent.type(emailInput, loginBody.email);
-    userEvent.type(passwordInput, loginBody.password);
-
-    // Submit form
-    const submitButton = screen.getByRole('button', { name: '登入' });
-    userEvent.click(submitButton);
-
-    // Check sign in, sign up button to be disabled
-    // const spinningButton = await screen.findByRole('button', { name: 'spining' });
-    // const signupButton = screen.getByRole('button', { name: '註冊' });
-    // expect(spinningButton).toBeDisabled();
-    // expect(signupButton).toBeDisabled();
-
-    // Check that postLogin function was called with correct arguments
-    const actions = store.getActions();
-    expect(actions).toContainEqual(userLogin.pending('login padding', loginBody));
-    expect(actions).toContainEqual(userLogin.fulfilled(
-      loginResponse,
-      'login fulfilled',
-      loginBody,
-    ));
+    expect(screen.getByRole('heading', { name: '登入' })).not.toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'Profile' })).toBeInTheDocument();
   });
 });
