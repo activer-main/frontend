642ed056094358ab004366be0c0fdb02f60959f7
GeorgeCheng1023 <ck40292@gmail.com>
2023-04-07 18:51:35 +0800

    feat: header logo link

--- src/components/Header/Logo/index.tsx
+++ src/components/Header/Logo/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Logo() {
   return (
-    <div className="logo">
+    <Link className="logo" to="/">
       <img src="/icon192.png" alt="activer-header-logo" width={64} />
-    </div>
+    </Link>
   );
 }
 
