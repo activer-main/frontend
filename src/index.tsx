4f364359f9083b48664756bead229fd2185ef296
GeorgeCheng1023 <ck40292@gmail.com>
2023-04-08 16:31:08 +0800

    feat: login mui

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,6 @@ import routerConfig from 'utils/router';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from 'store';
-import { ThemeProvider } from '@mui/material';
-import { theme } from 'styles/theme';
 import './index.scss';
 
 const router = createBrowserRouter(routerConfig);
@@ -14,9 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <RouterProvider router={router} />
-      </ThemeProvider>
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>,
 );
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,76 +1,112 @@
-import React, { useEffect } from 'react';
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 import { useAppDispatch, useAppSelector } from 'store';
 import { userLogin } from 'store/auth/authAction';
+import { toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
-import Button from 'components/Button';
-import FormInput from 'components/FormInput';
-import { LoginFormDataType } from 'types/user';
-import './index.scss';
-import { PASSWORD_PATTERN } from 'utils/pattern';
+import LoadingButton from '@mui/lab/LoadingButton';
 
-function Login() {
-  const { loading, userInfo } = useAppSelector((state) => state.auth);
-  const navigate = useNavigate();
+export default function Login() {
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const { loading, userInfo } = useAppSelector((state) => state.auth);
 
-  // redirect authenticated user to profile screen
-  useEffect(() => {
-    if (userInfo) {
+  React.useEffect(() => {
+    if (!userInfo) {
       navigate(-1);
     }
-  }, [navigate, userInfo]);
+  }, []);
 
-  const handleSubmit = (event: React.SyntheticEvent) => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const target = event.target as typeof event.target & LoginFormDataType;
+    const data = new FormData(event.currentTarget);
+
     dispatch(userLogin({
-      email: target.email.value,
-      password: target.password.value,
-    }));
+      email: data.get('email') as string,
+      password: data.get('password') as string,
+    }))
+      .unwrap()
+      .then(() => {
+        navigate('/user/profile');
+      })
+      .catch((error: any) => {
+        toast(error.message);
+      });
   };
 
   return (
-    <form onSubmit={handleSubmit} className="login">
-      <h1 className="login__header">登入</h1>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar sx={{ m: 1 }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          登入
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="帳戶"
+            name="email"
+            autoComplete="email"
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="密碼"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
 
-      <FormInput
-        className="login__account"
-        name="email"
-        label="帳號"
-        type="email"
-        title="請輸入正確帳號格式"
-        required
-      />
+          <LoadingButton
+            loading={loading}
+            loadingPosition="end"
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            登入
+          </LoadingButton>
 
-      <FormInput
-        className="login__password"
-        name="password"
-        label="密碼"
-        type="password"
-        pattern={PASSWORD_PATTERN}
-        title="密碼至少八位字元，需要包含至少一個數字、一個大寫英文、一個小寫英文、一個特殊字元: !@#$%"
-        required
-      />
+          <Grid container>
+            <Grid item xs>
+              <Link href="/" variant="body2">
+                忘記密碼?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="/register" variant="body2">
+                立即註冊
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
 
-      <div className="login__control">
-        <Button
-          type="submit"
-          text={loading ? 'spining' : '登入'}
-          className="button"
-          disabled={loading}
-        />
-        <Button
-          type="button"
-          text="註冊"
-          variant={{ outline: true }}
-          className="button"
-          disabled={loading}
-          onClick={() => navigate('/register')}
-        />
+    </Container>
 
-      </div>
-
-    </form>
   );
 }
-export default Login;
